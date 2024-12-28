898b6cbf5709752022f174ca30608df3dc41c396
Jana <janagt354@gmail.com>
2024-12-28 22:32:10 +0530

    Javascript

--- JavaScript/controlStatements.js
+++ JavaScript/controlStatements.js
@@ -1,7 +1,74 @@
+console.log("ASSIGN OPERATOR")
 a=10
-if(a%2==0){
-    console.log(a, "is Even");
+b=15
+c=5
+console.log("ADDITION")
+console.log(a+b)
+console.log("SUBTRACTION")
+console.log(a-b)
+console.log("MULTIPLICATION")
+console.log(a*b)
+console.log("DIVISION")
+console.log(a/b)
+console.log("MODULE")
+console.log(a%b)
+console.log("AND OPERATOR")
+
+
+if(a>b && a>c)
+{
+    console.log("a is greater")
+
+}
+else if(b>c)
+{
+    console.log("b is greater")
 }
 else{
-    console.log(a,"is Odd");
+    console.log("c is greater")
+}
+console.log("NOT OPERATOR")
+if(a!=b)
+{
+    console.log("a is not equal to b")
+}
+else{
+    console.log("a is  equal to b")
+}
+console.log("OR OPERATOR")
+if(a<b||a<c)
+{
+    console.log(a)
+}
+else{
+    console.log(b)
+}
+console.log("SHIFT OPERATOR")
+ console.log(a<<2)
+ console.log(b<<2)
+console.log("INCREMENT OPERATOR")
+console.log(a++,"and",a--)
+console.log(b++,"and",b--)
+console.log("CAP OPERATOR")
+console.log(a^5)
+console.log(b^5)
+console.log("DO WHILE")
+let i=0
+let text=""
+do{
+    i++
+    console.log(i)
+}
+while(d<5)
+    console.log("FOR IN AND FOR OF OPERATOR")
+var arr = [3, 5, 7];
+arr.foo = "hello";
+console.log("RESULT FOR IN")
+for (var d in arr) {
+  console.log(i); 
+}
+console.log("RESULT FOR OF") 
+for (var d of arr) {
+   
+  console.log(d); 
 }
